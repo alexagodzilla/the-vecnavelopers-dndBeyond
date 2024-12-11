edb374f8263c48cd4110878e083417e7c9cbe3df
Valeria <valeria.serci.uk@gmail.com>
2024-12-11 21:25:50 +0000

    UserControllerTest passing; Valeria and Alessandro

---
+++ src/test/java/com/vecnavelopers/dndbeyond/controller/UserControllerTest.java
@@ -0,0 +1,54 @@
+package com.vecnavelopers.dndbeyond.controller;
+
+import com.vecnavelopers.dndbeyond.model.User;
+import com.vecnavelopers.dndbeyond.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserControllerTest {
+    @InjectMocks
+    private UserController userController;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private SecurityContextHolder securityContextHolder;
+
+    @Test
+    public void testAfterLogin_NewUser() {
+
+        DefaultOidcUser mockUser = mock(DefaultOidcUser.class);
+        when(mockUser.getAttributes()).thenReturn(Map.of("email", "test@example.com"));
+
+        SecurityContext securityContext = mock(SecurityContext.class);
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.getPrincipal()).thenReturn(mockUser);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+
+        when(userRepository.findUserByUsername("test@example.com")).thenReturn(Optional.empty());
+        when(userRepository.save(any(User.class))).thenReturn(new User("test@example.com"));
+
+        RedirectView result = userController.afterLogin();
+
+        verify(userRepository).save(any(User.class));
+        assertEquals("/", result.getUrl());
+    }
+
+}
