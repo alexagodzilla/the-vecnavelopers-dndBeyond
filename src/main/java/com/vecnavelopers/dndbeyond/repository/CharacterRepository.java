5d4eb0a5ea5626f4cb60a9ae2c1edfd17ad334b6
“alexagodzilla” <alexagozzino@gmail.com>
2024-12-12 15:47:17 +0000

    Character imported into Repository after rebuilding the app

--- src/main/java/com/vecnavelopers/dndbeyond/repository/CharacterRepository.java
+++ src/main/java/com/vecnavelopers/dndbeyond/repository/CharacterRepository.java
@@ -1,6 +1,10 @@
 package com.vecnavelopers.dndbeyond.repository;
 
+import com.vecnavelopers.dndbeyond.model.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CharacterRepository extends JpaRepository<Character, Long> {
+import java.util.List;
+import java.util.Optional;
+
+public interface CharacterRepository extends JpaRepository<Character, Long>{
 }
