ab11d70c70d3038067436cbe46d076c485ea637b
Valeria <valeria.serci.uk@gmail.com>
2024-12-12 11:14:58 +0000

    add SpringBootConfiguration and before each using flyway to clean the db

--- src/test/java/com/vecnavelopers/dndbeyond/DndbeyondApplicationTests.java
+++ src/test/java/com/vecnavelopers/dndbeyond/DndbeyondApplicationTests.java
@@ -1,12 +1,24 @@
 package com.vecnavelopers.dndbeyond;
 
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@SpringBootTest
+@SpringBootTest(properties = "spring.flyway.clean-disabled=false")
 class DndbeyondApplicationTests {
+
+	@Autowired
+	private Flyway flyway;
+
+	@BeforeEach
+	void setUpDatabase() {
+		flyway.clean();
+		flyway.migrate();
+	}
 
 	@Test
 	void contextLoads() {
