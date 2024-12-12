6e5fd39e05f682b6d4bff1fbf588587f643e6726
“alexagodzilla” <alexagozzino@gmail.com>
2024-12-12 15:13:57 +0000

    Create Character Repository Alessandro Agozzino Adam Hussain

---
+++ src/test/java/com/vecnavelopers/dndbeyond/repository/CharacterRepository.java
@@ -0,0 +1,6 @@
+package com.vecnavelopers.dndbeyond.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CharacterRepository extends JpaRepository<Character, Long> {
+}
