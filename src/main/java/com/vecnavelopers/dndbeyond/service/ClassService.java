a2e149361eac42211c657db0877950836e162900
Matt Rowell <matthewjrowell@icloud.com>
2024-12-15 17:16:15 +0000

    DndApiService renamed to ClassService to reflect its sole purpose of collecting class information. Associated files referencing the file have been amended to match the new name change. Deleted the redundant DndApiController file as its functions were moved to other controllers. Utility folder added to house the new AbilityScoreMapper class, which replaces ability shorthand with the full name if called. Added 404.html file from Acebook so that we can customise error pages.

--- src/main/java/com/vecnavelopers/dndbeyond/service/ClassService.java
+++ src/main/java/com/vecnavelopers/dndbeyond/service/ClassService.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class DndApiService {
+public class ClassService {
 
     private final OkHttpClient client = new OkHttpClient();
     private final ObjectMapper objectMapper = new ObjectMapper();
