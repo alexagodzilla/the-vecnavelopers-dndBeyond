45e8968827f827b430af1ca88493b5053bf26e0a
Matt Rowell <matthewjrowell@icloud.com>
2024-12-16 20:11:56 +0000

    Amended details templates and repositories for extra details so that the extra info should be display now. Issue with information being displayed when clicking on the 'See Class Details' buttons on selection screen.

--- src/main/java/com/vecnavelopers/dndbeyond/controller/ClassController.java
+++ src/main/java/com/vecnavelopers/dndbeyond/controller/ClassController.java
@@ -2,6 +2,8 @@ package com.vecnavelopers.dndbeyond.controller;
 
 
 import com.vecnavelopers.dndbeyond.model.ClassDetails;
+import com.vecnavelopers.dndbeyond.model.ClassExtraDetails;
+import com.vecnavelopers.dndbeyond.repository.ClassExtraDetailsRepository;
 import com.vecnavelopers.dndbeyond.service.ClassService;
 
 import org.springframework.stereotype.Controller;
@@ -14,15 +16,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class ClassController {
 
     private final ClassService classService;
+    private final ClassExtraDetailsRepository classExtraDetailsRepository;
 
-    public ClassController(ClassService classService) {
+    public ClassController(ClassService classService, ClassExtraDetailsRepository classExtraDetailsRepository) {
         this.classService = classService;
+        this.classExtraDetailsRepository = classExtraDetailsRepository;
     }
 
     @GetMapping("/classes/{className}")
     public String getClassDetails(@PathVariable String className, Model model) {
         ClassDetails classDetails = classService.getClassDetails(className);
         model.addAttribute("classDetails", classDetails);
+        ClassExtraDetails extraDetails = classExtraDetailsRepository.findByClassName(className);
+        model.addAttribute("extraDetails", extraDetails);
         return "class-details";
     }
 }
--- src/main/java/com/vecnavelopers/dndbeyond/controller/SpeciesController.java
+++ src/main/java/com/vecnavelopers/dndbeyond/controller/SpeciesController.java
@@ -1,6 +1,8 @@
 package com.vecnavelopers.dndbeyond.controller;
 
 import com.vecnavelopers.dndbeyond.model.SpeciesDetails;
+import com.vecnavelopers.dndbeyond.model.SpeciesExtraDetails;
+import com.vecnavelopers.dndbeyond.repository.SpeciesExtraDetailsRepository;
 import com.vecnavelopers.dndbeyond.service.SpeciesService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,13 +15,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SpeciesController {
 
     private final SpeciesService speciesService;
+    private final SpeciesExtraDetailsRepository speciesExtraDetailsRepository;
 
-    public SpeciesController(SpeciesService speciesService) { this.speciesService = speciesService; }
+    public SpeciesController(SpeciesService speciesService, SpeciesExtraDetailsRepository speciesExtraDetailsRepository) { this.speciesService = speciesService;
+        this.speciesExtraDetailsRepository = speciesExtraDetailsRepository;
+    }
 
     @GetMapping("/{speciesName}")
     public String getSpeciesDetails(@PathVariable String speciesName, Model model) {
         SpeciesDetails speciesDetails = speciesService.getSpeciesDetails(speciesName);
         model.addAttribute("speciesDetails", speciesDetails);
+        SpeciesExtraDetails extraDetails = speciesExtraDetailsRepository.findBySpeciesName(speciesName);
+        model.addAttribute("extraDetails", extraDetails);
         return "species-details";
     }
 }
